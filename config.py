e0b29295f9bb27989cd865bdbf85aca0adc155ae
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-19 18:42:28 +0300

    create table

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://audrey:12345@localhost/pitch_test'
 
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://audrey:12345@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://audrey:12345@localhost/pitches'
